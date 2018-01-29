cf3a0557aa1e02c9c4709472d53dd90a747397e0
Daniel <30894056+3Cement@users.noreply.github.com>
2018-01-29 20:01:56 +0000

    Create encrypting_all_PDFs2.py

---
+++ encrypting_all_PDFs2.py
@@ -0,0 +1,44 @@
+#! python3
+# encrypting_all_PDFs2.py - script that will go through every PDF in a folder
+# and encrypt the PDFs using a provided password. 
+
+import os, PyPDF2, time, logging
+
+def encryptingPDFs(password):
+	start_time = time.time()
+	logging.getLogger().setLevel(logging.INFO)
+	#parser_logger.setLevel(logging.INFO)
+
+	for foldername, subfolders, filenames in os.walk('.'):
+		print('The current folder is ' + foldername)
+
+		for file in filenames:
+			# Finds PDF files
+			if file.endswith('.pdf'):
+				filename = os.path.abspath(os.path.join(foldername, file))
+				logging.info('Reading file: ' + str(filename))
+				pdfFile = open(filename, 'rb')
+				pdfReader = PyPDF2.PdfFileReader(pdfFile)
+				if not pdfReader.isEncrypted:
+					pdfWriter = PyPDF2.PdfFileWriter()
+					# Copy every page
+					for pageNum in range(pdfReader.numPages):
+						pdfWriter.addPage(pdfReader.getPage(pageNum))	
+					# Encrypt each PDF file			
+					pdfWriter.encrypt(password)
+					logging.info("File " + str(filename) + " has been successfully encrypted")
+					# Save file
+					name = (os.path.basename(filename)).split(".")[0]
+					newName = os.path.join(foldername, "encrypted_" + name + ".pdf")
+					newName = os.path.abspath(newName)
+					resultPdf = open(newName, 'wb')
+					pdfWriter.write(resultPdf)
+					logging.info("File has been renamed to: " + str(newName))
+					resultPdf.close()
+					pdfFile.close()
+
+	end_time = time.time()
+	print("\n--- %s seconds ---" % round((end_time - start_time),2))
+
+encryptingPDFs('kiki')
+
